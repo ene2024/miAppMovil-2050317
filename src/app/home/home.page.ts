cf91988456b9ebdc0ccc734cbbfe4401631e4526
thernandez3377@gmail.com <[thernandez3377@gmail.com]>
2024-03-23 18:04:18 -0600

    Home.page.html

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck, AfterContentInit, 
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,40 @@ export class HomePage {
   verdadero: boolean = true; // Definición de la propiedad verdadero
   title: string = 'Mi nueva pagina!';
   imgUrl: string = 'https://i.redd.it/7ksiaa4exhh01.gif'
-  constructor() {}
+  constructor() { 
+    console.log("AppComponent: Constructor");
+  }
+  ngOnChanges() {
+    console.log("AppComponent: OnChanges");
+  }
+
+  ngOnInit() {
+    console.log("AppComponent: OnInit");
+  }
+
+  ngDoCheck() {
+    console.log("AppComponent: OnChanges");
+  }
+
+  ngAfterContentInit() {
+    console.log("AppComponent: AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AppComponent: AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("AppComponent: AfterContentViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AppComponent: AfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("AppComponent: OnDestroy");
+  }
   hazmeClic(): void{
     alert("hiciste clic!");
   }
